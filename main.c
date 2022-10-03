bd575882272d2183afdce0119f87ec1e4c7fae1a
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-10-03 15:44:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Poivret Julien Oct 3 2022 poivretjulien@gmail.com
+
+
 /*
-  
-  Poivret Julien Oct 3 2022 poivretjulien@gmail.com
   
            Quick and simple fundammantal for advanced coding in C. 
     
