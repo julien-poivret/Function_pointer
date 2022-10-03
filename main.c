6c15c9a935070161ab57bb17a0b1558a97725af5
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-10-03 15:29:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 /*
-  Quick and simple fundammantal for advanced coding in C.
+           Quick and simple fundammantal for advanced coding in C. 
     
   This technic is a practical way to store user code variations in a data structure     
     possibly configured by a fonction for this effect and run by another one.      
